5e8edec329b9bbe6eb3c7b00dcf80f62866d8955
Herivasm <herihernandez6@gmail.com>
2025-10-10 07:58:55 -0700

    feat: add environment configuration and validation, integrate modules
    - Added dotenv and joi for environment variable management and validation.
    - Created a new config file to validate and load environment variables.
    - Implemented global validation pipes in the main application bootstrap process.
    - Updated package.json to include new dependencies.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CategoriesModule } from './categories/categories.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { ProductsModule } from './products/products.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule, SuppliersModule, ProductsModule, SalesModule],
   controllers: [],
   providers: [],
 })
