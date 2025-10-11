b241b825e3f15d7a209a12eeecd4371629542c76
Herivasm <herihernandez6@gmail.com>
2025-10-11 10:15:35 -0700

    feat: remove SalesModule from AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { CategoriesModule } from './categories/categories.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
 import { ProductsModule } from './products/products.module';
-import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [CategoriesModule, SuppliersModule, ProductsModule, SalesModule],
+  imports: [CategoriesModule, SuppliersModule, ProductsModule],
   controllers: [],
   providers: [],
 })
